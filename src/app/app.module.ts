220cc7ab05a272f7545f62c64bf779efa4c12ba2
Pablo Cardoso <pablo.cardoso@hotmail.com>
2023-06-06 18:18:02 -0300

    upgrade na CSS (ainda sem responsividade)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { HeaderComponent } from './header/header.component';
     LoginComponent,
     HomeComponent,
     MovieComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
